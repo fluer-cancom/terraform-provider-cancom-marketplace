66148d4c6cf5593f01c7dbeeceee7172a6cfc90c
Bernhard Flür <bernhard.fluer@cancom.com>
2026-07-17 12:39:58 +0200

    feat: Update example subscription resource with azure_owner_object_id

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,6 @@
 resource "cancom-marketplace_az_subscription" "example" {
-  display_name = "CC Test Subscription"
+  display_name          = "CC Test Subscription"
+  azure_owner_object_id = "00000000-0000-0000-0000-000000000000"
 }
 
 output "subscription_id" {
